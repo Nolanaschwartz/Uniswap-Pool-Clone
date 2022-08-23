3e1322f6735711b656f3c8ef84717fd7bb665623
Nolan <nolan.a.schwartz@gmail.com>
2022-08-23 04:36:58 -0700

    Add 10 sec ETH price polling

--- uniswap-pool-clone/components/Pool/DetailCard.tsx
+++ uniswap-pool-clone/components/Pool/DetailCard.tsx
@@ -3,37 +3,42 @@ import { Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 import { formatUSD } from "../../util/util";
 import { useQuery } from "@apollo/client";
 import ETHPrice from "../../gql/ETHPrice.gql";
+import { useMemo } from "react";
 
 const DetailCard = () => {
   const { data } = usePool();
-  const { data: ETHData } = useQuery(ETHPrice);
-  if (!data || !ETHData) {
-    return undefined;
-  }
-  const { ethPriceUSD } = ETHData.bundle;
+  const { data: ETHData, startPolling } = useQuery(ETHPrice, {
+    pollInterval: 10000,
+    ssr: false,
+  });
+  const { ethPriceUSD } = ETHData?.bundle || 0;
+  startPolling(10000);
 
-  return (
-    <GridItem colSpan={4} padding={"8px"}>
-      <Grid templateColumns="repeat(12, 1fr)" gap={1}>
-        <GridItem colSpan={9}>
-          <Heading as="h3" size="md">
-            Tokens Value (USD)
-          </Heading>
-          <Text>{`${data?.pool?.token0.symbol}: ${formatUSD(
-            data?.pool?.token0.derivedETH * ethPriceUSD
-          )}`}</Text>
-          <Text>{`${data?.pool?.token1.symbol}: ${formatUSD(
-            data?.pool?.token1.derivedETH * ethPriceUSD
-          )}`}</Text>
-        </GridItem>
-        <GridItem colSpan={3}>
-          <Heading as="h3" size="md">
-            TX Count
-          </Heading>
-          <Text>{data?.pool?.txCount}</Text>
-        </GridItem>
-      </Grid>
-    </GridItem>
+  return useMemo(
+    () => (
+      <GridItem colSpan={4} padding={"8px"}>
+        <Grid templateColumns="repeat(12, 1fr)" gap={1}>
+          <GridItem colSpan={9}>
+            <Heading as="h3" size="md">
+              Tokens Value (USD)
+            </Heading>
+            <Text>{`${data?.pool?.token0.symbol}: ${formatUSD(
+              data?.pool?.token0.derivedETH * ethPriceUSD
+            )}`}</Text>
+            <Text>{`${data?.pool?.token1.symbol}: ${formatUSD(
+              data?.pool?.token1.derivedETH * ethPriceUSD
+            )}`}</Text>
+          </GridItem>
+          <GridItem colSpan={3}>
+            <Heading as="h3" size="md">
+              TX Count
+            </Heading>
+            <Text>{data?.pool?.txCount}</Text>
+          </GridItem>
+        </Grid>
+      </GridItem>
+    ),
+    [data, ethPriceUSD]
   );
 };
 
